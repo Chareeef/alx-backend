3cf7d4643e1ed8931db27bcda31afc98c99780a9
Chareeef <youssef.charif.h@gmail.com>
2024-04-02 13:49:51 +0000

    Implement LIFO Caching System

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" LIFOCache: Cache with Last In First Out implementation
+"""
+from base_caching import BaseCaching
+from collections import deque
+
+
+class LIFOCache(BaseCaching):
+    """ A LIFO caching system
+    """
+
+    def __init__(self):
+        """Initialize the caching system
+        """
+        super().__init__()
+        self.last_in_keys = deque()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key and item:
+            # If the cache is full, Remove the first in element
+            if len(self.cache_data) == self.MAX_ITEMS\
+                    and key not in self.cache_data:
+                key_to_remove = self.last_in_keys.popleft()
+                self.cache_data.pop(key_to_remove)
+                print('DISCARD:', key_to_remove)
+
+            # Update cache
+            self.cache_data[key] = item
+
+            # If the key was already in last_in_keys, remove it first
+            if key in self.last_in_keys:
+                self.last_in_keys.remove(key)
+
+            # Enqueue the key to last_in_keys queue
+            self.last_in_keys.appendleft(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
