481250b7f5c28ba42ddce0503a59d199696a1281
Chareeef <youssef.charif.h@gmail.com>
2024-04-09 16:37:20 +0000

    Don't import _

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """A Flask application"""
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 # Create the app
 app = Flask(__name__)
