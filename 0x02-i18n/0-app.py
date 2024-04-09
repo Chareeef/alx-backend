d0ca6cd1eabedb95df9cef90daf17849381d73e3
Chareeef <youssef.charif.h@gmail.com>
2024-04-09 12:26:41 +0000

    Start a Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""A Flask application"""
+from flask import Flask, render_template
+
+# Create the app
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """The Index route"""
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
