1a98a5ec9dc3e8ef41afd36c3755f6e750f4789a
Chareeef <youssef.charif.h@gmail.com>
2024-04-09 12:30:35 +0000

    Fix pycodestyle

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 # Create the app
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """The Index route"""
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run()
